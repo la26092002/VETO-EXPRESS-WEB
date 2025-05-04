8b87f1d54a6477ee45c4c1fc758990ee02a27813
la26092002 <larbibenyakhou@gmail.com>
2025-05-04 11:50:27 +0200

    fix error

--- src/routes/livreur.js
+++ src/routes/livreur.js
@@ -5,7 +5,7 @@ const { verifyToken, isValidate, isClient, isBan, isLivreur } = require('../midd
 
 
 router.get('/AfficherServiceConsultations', verifyToken, isValidate, isLivreur, isBan, livreurController.AfficherServiceConsultations);
-router.get('/modifierStatusServicesVenteParUser', verifyToken, isValidate, isLivreur, isBan, livreurController.modifierStatusServicesVenteParUser);
+router.put('/modifierStatusServicesVenteParUser', verifyToken, isValidate, isLivreur, isBan, livreurController.modifierStatusServicesVenteParUser);
 
 
 
